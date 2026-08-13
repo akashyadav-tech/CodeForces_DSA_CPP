9defa63885a4752891516ac7e49551f202520ef1
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-08-14 00:41:33 +0530

    Add Chewbaсca and Number [A] from Codeforces

---
+++ 514/A - Chewbaсca and Number/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string s;
+    cin >> s;
+ 
+    for(int i = 0; i < s.size(); i++) {
+        int d = s[i] - '0';
+        int inv = 9 - d;
+ 
+        if(i == 0 && inv == 0)
+            continue;
+ 
+        if(inv < d)
+            s[i] = char(inv + '0');
+    }
+ 
+    cout << s;
+ 
+    return 0;
+}
