74344bbb18f50216bb074a470760c9e0c520f8b8
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-08-16 23:21:32 +0530

    Add In Search of an Easy Problem [A] from Codeforces

---
+++ 1030/A - In Search of an Easy Problem/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+   int n;
+   cin>>n;
+  int hard;
+  string ans="EASY";
+ 
+   for(int i=0;i<n;i++){
+    int x;
+    cin>>x;
+    if(x==1){
+        ans="HARD";
+        break;
+    } 
+   }
+ 
+   cout<<ans;
+ 
+      cout<<"
+";
+    
+    return 0;
+}
