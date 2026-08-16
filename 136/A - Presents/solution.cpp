77281ebd7e92ff229cd367d9f0afc613005c6be2
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-08-16 23:45:02 +0530

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+   int n;
+   cin>>n;
+   map<int,int> mp;
+   for(int i=1; i<=n;i++){
+    int x;
+    cin>>x;
+    mp[x]=i;
+   }
+ 
+  for(int i=1; i<=n;i++){
+    cout<<mp[i]<<" ";
+  }
+   
+ 
+      cout<<"
+";
+    
+    return 0;
+}
