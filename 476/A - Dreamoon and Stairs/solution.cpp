7fcc0e5d210bdae4bc2baa84e864b9c407c9d194
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-08-28 21:54:42 +0530

    Add Dreamoon and Stairs [A] from Codeforces

---
+++ 476/A - Dreamoon and Stairs/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    int minMoves = (n + 1) / 2;
+    
+    for (int i = minMoves; i <= n; i++) {
+        if (i % m == 0) {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    
+    cout << -1 << endl;
+    return 0;
+}
